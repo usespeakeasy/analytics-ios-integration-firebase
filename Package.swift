d7fbbf115a5e4eb539dad811a96ede901aab9444
Tyler Elko <150105290+TElkoSpeak@users.noreply.github.com>
2025-05-05 15:35:42 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
     platforms: [.iOS(.v11)],
     products: [.library(name: "SegmentFirebase", targets: ["SegmentFirebase"])],
     dependencies: [
-      .package(url: "https://github.com/usespeakeasy/analytics-ios", branch: "master"),
-      .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.23.0"),
+      .package(url: "https://github.com/usespeakeasy/analytics-ios", from: "1.0.0"),
+      .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.23.0"),
     ],
     targets: [
         .target(
